a9ecbd954a08b4c392061e0c550a6f567b730e3b
RonaldoMPF <ronaldo.medeiros.filho@ccc.ufcg.edu.br>
2017-11-09 15:42:39 -0300

    Sprint02 - add driver and vehicle entitys.

---
+++ src/main/java/com/server/entities/Driver.java
@@ -0,0 +1,64 @@
+package com.server.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "driver")
+public class Driver implements Serializable{
+	
+	private Long id;
+	
+	private String name;
+	
+	private String cpf;
+	
+	private String cnh;
+
+	public Driver() {
+		
+	}
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getCnh() {
+		return cnh;
+	}
+
+	public void setCnh(String cnh) {
+		this.cnh = cnh;
+	}
+	
+	
+	
+
+}
