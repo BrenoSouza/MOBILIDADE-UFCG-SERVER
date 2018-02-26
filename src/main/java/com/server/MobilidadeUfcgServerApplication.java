e29dd1608a0c03525bdc41d0cbc0fce13d1cb1cf
raimundo.miranda@ccc.ufcg.edu.br <raimundo.miranda@ccc.ufcg.edu.br>
2018-02-26 13:32:53 -0300

    swagger docmuntation token authorize

--- src/main/java/com/server/MobilidadeUfcgServerApplication.java
+++ src/main/java/com/server/MobilidadeUfcgServerApplication.java
@@ -1,5 +1,6 @@
 package com.server;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.TimeZone;
 
@@ -18,6 +19,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger.web.ApiKeyVehicle;
@@ -38,11 +40,11 @@ public class MobilidadeUfcgServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MobilidadeUfcgServerApplication.class, args);
 	}
-	
+	/*
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build().apiInfo(apiInfo());
-	}
+	}*/
 	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title("Mobilidade UFCG")
@@ -59,18 +61,19 @@ public class MobilidadeUfcgServerApplication {
         System.out.println("Spring boot application running in UTC timezone :"+new Date());   // It will print UTC timezone
     }
 	
-	/*@Bean
-	public SecurityConfiguration security() {
-		String token;
-		try {
-			UserDetails userDetails = this.userDetailsService.loadUserByUsername("admin@admin.com");
-			token = this.jwtTokenUtil.obterToken(userDetails);
-		} catch (Exception e) {
-			token = "";
-		}
-
-		return new SecurityConfiguration(null, null, null, null, "Bearer " + token, ApiKeyVehicle.HEADER,
-				"Authorization", ",");
-	}*/
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+          .select()
+          .apis(RequestHandlerSelectors.basePackage("com.server"))
+          .paths(PathSelectors.any())
+          .build()
+          .apiInfo(apiInfo())
+          .securitySchemes(Arrays.asList(apiKey()));
+    }
+	
+	 private ApiKey apiKey() {
+	        return new ApiKey("Authorization", "Authorization", "header");
+	      }
 
 }
